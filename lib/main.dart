dee863f10b155fbd84cb112ed9f7ddb39a6bbb5d
Robin Sardja <robinsardja@gmail.com>
2023-10-20 23:47:58 -0400

    First iteration of workshop project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,58 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
+import 'settings.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+
+  static const List<Widget> routes = [ Home(), Settings() ];
+
+  int currentIndex = 0;
+  void changeIndex(index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text("My USF GDSC App"),
+        ),
+        body: routes[currentIndex],
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.thumb_up),
+          onPressed: () => {
+            
+          },
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "Settings",
+            ),
+          ],
+          currentIndex: currentIndex,
+          onTap: (index) => changeIndex(index),
         ),
       ),
     );
